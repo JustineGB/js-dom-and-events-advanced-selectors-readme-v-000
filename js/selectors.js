3433269a42a1aea8e23e7c4eb894c05ce7a86af7
Justine B <justinegayton@gmail.com>
2019-03-11 22:36:17 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,12 +1,3 @@
-// declare your functions here...
-// $('img');
-// $('.pics');
-// $('#baby-ninja');
-// $('ul li');
-// $('parent-tag child-tag:first-child');
-// $('div img:first-child');
-// $("img[alt='the beatles making faces']");
-
 function paragraphSelector(){
   return $('p');
 }
